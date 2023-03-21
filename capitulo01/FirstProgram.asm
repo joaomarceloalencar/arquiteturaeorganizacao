5dac5592132fbb2000a7c66c52f039d991fbe5c8
joaomarceloalencar <joao.marcelo@ufc.br>
2023-03-21 15:31:46 -0300

    Programas do Capítulo 01

---
+++ capitulo01/FirstProgram.asm
@@ -0,0 +1,19 @@
+;
+;   Building Instructions:
+;   ml /Cx /coff FirstProgram.asm /link /SUBSYSTEM:console /out:FirstProgram.exe kernel32.lib legacy_stdio_definitions.lib msvcrt.lib
+;
+
+.686
+.model  flat, c 
+.stack  100h 
+.data
+num1    sdword ?            ; first number
+num2    sdword ?            ; second number
+.code
+main    proc
+        mov     num1,5      ; initialize num1 with 5
+        mov     eax,num1    ; load eax with contents of num1
+        mov     num2,eax    ; store eax in num2
+        ret
+main    endp 
+end
---
+++ capitulo01/FirstProgram.c
@@ -0,0 +1,13 @@
+/*
+    Building Instructions:
+    ml.exe FirstProgram.c
+*/
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2;
+    num1 = 5;
+    num2 = num1;
+    return 0;
+}
